6a95938f109076ead384b204864fa7d3bf299b20
Om Bilaiya <ombilaiya01@gmail.com>
2025-11-25 20:02:37 +0530

    Remove Firebase configuration from firebase-config.js
    Removed Firebase configuration details from the file.

--- public/js/config/firebase-config.js
+++ public/js/config/firebase-config.js
@@ -3,16 +3,6 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
 
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyC2hADJIFOHR_qAMB-kgsaRnBDQ7xPuj98",
-    authDomain: "athelete-management-c991a.firebaseapp.com",
-    projectId: "athelete-management-c991a",
-    storageBucket: "athelete-management-c991a.firebasestorage.app",
-    messagingSenderId: "1095205765222",
-    appId: "1:1095205765222:web:8651aa1ee1341254452da7",
-    measurementId: "G-J2Y006DEYM"
-  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
